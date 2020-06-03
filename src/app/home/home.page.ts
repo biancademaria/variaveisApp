499f8c6e45562516c84603490db70088c927f41d
biancademaria <bianca.maria@etec.sp.gov.br>
2020-06-03 15:00:47 -0300

    variaveisApp completo
    de acordo com a aula postada pelo ronan no youtube, do dia 02/06/2020

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,42 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  public mensagem;
 
+  public nome: string; // armazenar textos.
+  public idade: number; // armazenar números (inteiros E decimais)
+  public excluido: boolean; //armazena TRUE ou FALSE
+
+  public situacao: any; // armazena qualquer tipo de variável
+  public anoNascimento: number;
+
+  constructor() {
+    // alert("mensagem: " + this.mensagem) -- EM FORMA DE ALERTA (JANELA POP-UP)
+    // console.log("Mensagem: " + this.mensagem); -- NÃO VISÍVEL AO USUÁRIO
+
+    this.mensagem = "Olá a todos!";
+
+    console.log("Mensagem: " + this.mensagem);
+
+    // this.mensagem = 2020;
+  }
+
+  public saudacao() {
+    this.mensagem = "Olá, " + this.nome + "! Você informou a idade '" + this.idade + " anos'."
+    if (this.idade >= 18) {
+      this.situacao = "Você é maior de idade!";
+    }
+    else {
+      this.situacao = "Você é menor de idade!";
+    }
+  }
+
+  public anoDeNascimento() {
+    const anoAtual = 2020; // CONST: a variável não muda durante a execução do método
+    let anoNascimento = anoAtual - this.idade; // LET cria variáveis que podem ser alteradas durante a execução do método
+    this.anoNascimento = anoNascimento;
+  }
 }
